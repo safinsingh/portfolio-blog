2be6815079548aa1f3c507c1a3fcaef430ec2f51
Safin Singh <safin.singh@gmail.com>
2023-06-06 11:46:13 -0700

    publicize LV

--- src/util.ts
+++ src/util.ts
@@ -3,3 +3,12 @@ export const readTime = (rawContent: string) => {
 	const words = rawContent.trim().split(/\s+/).length;
 	return Math.ceil(words / wpm);
 };
+
+export const iframeFormat = () => {
+	const ratio = 16 / 9;
+
+	const youtubeIframes = Array.from(document.getElementsByClassName("youtube")) as HTMLIFrameElement[];
+	youtubeIframes.forEach((iframe) => {
+		iframe.style.height = `${Math.floor(iframe.offsetWidth / ratio)}px`;
+	});
+};
